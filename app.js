78e78df8a048915cd23ef3aaa390a1468f8f82fa
MattiaNatella <mattianatella@gmail.com>
2025-02-06 16:17:52 +0100

    [add] middlewares

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import express from 'express'
 import 'dotenv/config';
 import moviesRouter from './routers/movies.js'
-const app = express()
+import errorsHandler from './middlewares/errorsHandler.js';
+import notFound from './middlewares/notFound.js';
+import checkTime from './middlewares/checkTime.js';
 
+const app = express()
 const port = process.env.PORT || 3001;
+
+
+//middleware per l'apertura del server solo in determinati giorni ed orario
+app.use(checkTime)
 
 
 //serviamo ai client tutti i file dentro alla cartella public
@@ -17,10 +24,17 @@ app.get('/', (req, res) => {
     res.send('Sono la rotta del server movies')
 })
 
-//gestione rotte
-
+//rotta per i film
 app.use('/movies', moviesRouter)
 
+//middleware per la gestione degli errori
+app.use(errorsHandler)
+
+//middleware per la gestione delle pagine non trovate
+app.use(notFound)
+
+
+//metto in ascolto la porta
 app.listen(port, () => {
     console.log(`sono in ascolto alla porta http://${process.env.DB_HOST}:${port}/`)
 })
