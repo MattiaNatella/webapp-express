e202200902b96c7f12be40b125fca0a6349e7804
MattiaNatella <mattianatella@gmail.com>
2025-02-06 12:47:22 +0100

    [add] static public, connection to mysql database

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('ciao sono connesso al server')
+import express from 'express'
+const app = express()
+import 'dotenv/config';
+const port = process.env.PORT || 3001;
+
+
+//serviamo ai client tutti i file dentro alla cartella public
+app.use(express.static('public'))
+
+//Middleware per il parsing della body-request
+app.use(express.json())
+
+//Rotta risorsa server
+app.get('/', (req, res) => {
+    res.send('Sono la rotta del server movies')
+})
+
+//gestione rotte
+
+// app.use('/movies', moviesRouter)
+
+app.listen(port, () => {
+    console.log(`sono in ascolto alla porta http://${process.env.DB_HOST}:${port}/`)
+})
